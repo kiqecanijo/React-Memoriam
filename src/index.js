9cff98cc0179db61b11c4d949ae104c603a7cec6
kikin <enry1375@gmail.com>
2019-01-03 17:28:34 -0600

    adding redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { combineReducers, createStore } from 'redux'
+import { provider } from 'react-redux'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
