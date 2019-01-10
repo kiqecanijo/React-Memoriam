ef063ab8cf8f7078f680e30439385063c0759edf
kikin <enry1375@gmail.com>
2019-01-10 10:07:50 -0600

    attemping flip efect in cards

--- src/card.js
+++ src/card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { CSSTransitionGroup } from 'react-transition-group' // ES6
 
 const divStyle = {
   width: '25%',
@@ -13,16 +14,38 @@ const imgStyle = {
 }
 
 class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      element: 'X'
+    }
+  }
+
+  componentWillMount() {
+    setInterval(() => {
+      this.setState({
+        element: this.state.element == 'X' ? 'Y' : 'X'
+      })
+    }, 1000)
+  }
+
   render() {
+    /*const img = this.props.el.flipped ? (
+      <img alt="card" style={imgStyle} src={this.props.el.path} onClick={this.props.handleClick} />
+    ) : (
+      <img alt="card" style={imgStyle} src={'./static/gray.jpg'} onClick={this.props.handleClick} />
+    )*/
+
+    const img = this.state.element == 'X' ? <p>{this.state.element}</p> : <p>{this.state.element}</p>
+
     return (
-      <div style={divStyle}>
-        <img
-          alt="card"
-          style={imgStyle}
-          src={this.props.el.flipped ? this.props.el.path : './static/gray.jpg'}
-          onClick={this.props.handleClick}
-        />
-      </div>
+      <CSSTransitionGroup
+        style={divStyle}
+        transitionName="example"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        {img}
+      </CSSTransitionGroup>
     )
   }
 }
