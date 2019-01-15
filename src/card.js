ad84f317cc424f83ce4a1a3c9180907e7a0e4627
kikin <enry1375@gmail.com>
2019-01-15 11:27:02 -0600

    Added cards again, animation at start, buttons to interact

--- src/card.js
+++ src/card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { CSSTransitionGroup } from 'react-transition-group' // ES6
+import { CSSTransition, CSSTransitionGroup } from 'react-transition-group' // ES6
 
 const divStyle = {
   width: '25%',
@@ -30,20 +30,21 @@ class Card extends Component {
   }
 
   render() {
-    /*const img = this.props.el.flipped ? (
+    const img = this.props.el.flipped ? (
       <img alt="card" style={imgStyle} src={this.props.el.path} onClick={this.props.handleClick} />
     ) : (
       <img alt="card" style={imgStyle} src={'./static/gray.jpg'} onClick={this.props.handleClick} />
-    )*/
-
-    const img = this.state.element == 'X' ? <p>{this.state.element}</p> : <p>{this.state.element}</p>
+    )
 
     return (
       <CSSTransitionGroup
         style={divStyle}
         transitionName="example"
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={300}>
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={true}
+        transitionLeave={true}
+        classNames="fade">
         {img}
       </CSSTransitionGroup>
     )
